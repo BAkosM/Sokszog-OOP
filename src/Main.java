e4a8e27eead2a233f317535ac1073145ded5aa6f
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-10-05 12:17:02 +0200

    Main Add Test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            Trapez h = new Trapez();
+            Haromszog h = new Haromszog();
             System.out.println(h);
+        }
+        for (int i = 0; i < 5; i++) {
+            Paralelogramma p = new Paralelogramma();
+            System.out.println(p);
+        }
+        for (int i = 0; i < 5; i++) {
+            Trapez t = new Trapez();
+            System.out.println(t);
         }
     }
 }
